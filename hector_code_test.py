a44e771af89a5af90de5d793e74c7480078db409
Christos Spyropoulos <xristosandroid@gmail.com>
2017-04-26 16:11:40 +0200

    added the test file not the code file

--- hector_code_test.py
+++ hector_code_test.py
@@ -5,6 +5,7 @@ from hector_code import *
 class MyTest1(unittest.TestCase):
     def setUp(self):
         self.l1 = [[1, 2], [3, 4]]
+        self.l2 = [[5, 6], [7, 8]]
 
     def test_init(self):
         m1 = Matrix(self.l1)
@@ -13,8 +14,42 @@ class MyTest1(unittest.TestCase):
         self.assertEqual(m1.ncols, 2)
 
     def test_add(self):
-        pass
-# make a lot of tests
+        m1 = Matrix(self.l1)
+        m2 = Matrix(self.l2)
+        m3 = m1 + m2
+        self.assertEqual(m3.rows, [[6, 8], [10, 12]])
+        self.assertEqual(m3.nrows, 2)
+        self.assertEqual(m3.ncols, 2)
+
+    def test_sub(self):
+        m1 = Matrix(self.l1)
+        m2 = Matrix(self.l2)
+        m3 = m1 - m2
+        self.assertEqual(m3.rows, [[-4, -4], [-4, -4]])
+        self.assertEqual(m3.nrows, 2)
+        self.assertEqual(m3.ncols, 2)
+
+    def test_prod(self):
+        m1 = Matrix(self.l1)
+        m2 = Matrix(self.l2)
+        m3 = m1.prod(m2)
+        print m1
+        print m2
+        print m3
+        self.assertEqual(m3.rows, [[19, 22], [43, 50]])
+        self.assertEqual(m3.nrows, 2)
+        self.assertEqual(m3.ncols, 2)
+
+    def test_prod(self):
+        m1 = Matrix(self.l1)
+        matrix_string = str(m1)
+        self.assertEqual(matrix_string[0], "|")
+        self.assertEqual(matrix_string[1], " ")
+        self.assertEqual(matrix_string[2], "1")
+        self.assertEqual(matrix_string[3], " ")
+        self.assertEqual(matrix_string[4], "2")
+        self.assertEqual(matrix_string[5], " ")
+        self.assertEqual(matrix_string[6], "|")
+
 if __name__ == '__main__':
     unittest.main()
-
